9be443bd21e1d22ea7678b9328e35f30b88f2655
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-10-25 17:37:24 +0200

    Manged to toggle the items

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// hero image
+const heroProduct = document.querySelector(".hero-image");
+
 // Function to chnage image
 function chnageImage(e) {
   let image_source = e.target;
+  heroProduct.src = `${image_source.src}`;
   console.log(image_source.src);
 }
 // grabbing headsets from the DOM
