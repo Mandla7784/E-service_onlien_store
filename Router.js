a97a2042015d658d5c45c395c56e9c7bdae5b6fe
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-10-25 16:04:51 +0200

    added router function

--- Router.js
+++ Router.js
@@ -1,3 +1,14 @@
+//Function to navigate pages
 function showPage(pageId) {
-  const pages = document.querySelector(".page");
+  const pages = document.querySelectorAll(".page");
+  pages.forEach((page) => {
+    page.classList.remove("active"); // hiding all pages
+  });
+
+  const activePage = document.getElementById(pageId);
+  if (activePage) {
+    activePage.classList.add("active");
+  }
 }
+
+function router() {}
