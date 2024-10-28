3ef1c6c104615ef3e8313ca79e3e962991ae2bd2
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-10-28 17:58:37 +0200

    Fixed Dom load bug

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// hero image
-const heroProduct = document.querySelector(".hero-image");
-
-// Function to chnage image
-function chnageImage(e) {
-  let image_source = e.target;
-  heroProduct.src = `${image_source.src}`;
-  console.log(image_source.src);
-}
-// grabbing headsets from the DOM
-const headsets = document.querySelectorAll(".heads");
-headsets.forEach((headset) => {
-  headset.addEventListener("click", chnageImage);
-});
-
-// Log in FORM
 window.addEventListener("DOMContentLoaded", () => {
+  // hero image
+  const heroProduct = document.querySelector(".hero-image");
+
+  // Function to chnage image
+  function chnageImage(e) {
+    let image_source = e.target;
+    heroProduct.src = `${image_source.src}`;
+    console.log(image_source.src);
+  }
+  // grabbing headsets from the DOM
+  const headsets = document.querySelectorAll(".heads");
+  headsets.forEach((headset) => {
+    headset.addEventListener("click", chnageImage);
+  });
+  // Log in FORM
   const btnFormQuit = document.querySelector(".form-quite");
   btnFormQuit.addEventListener("click", function () {
     // whistle-page
