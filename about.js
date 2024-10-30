b3425e09892ecf7280b667bd2c4942055e4f429e
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2024-10-30 12:55:20 +0200

    Added render product details function

--- about.js
+++ about.js
@@ -1,26 +1,40 @@
 // Product Details
 
-function getProductDeatls(){
-    fetch("data.json")
+function getProductDetails(){
+
+ fetch("data.json")
     .then(resposne => {
         if(!resposne.ok){
             throw new Error("HTTP eror", resposne.status)
         }
+       
          return resposne.json()
-    })
-    .then(data => {
+    }).then(data => {
         console.log(data)
-    }).catch(error => {
-        console.log(error)
+         renderProductDetails(data)
     })
+ 
+
+
+    
 }
 
+function renderProductDetails(data){
+    const details =  data // product details list
+    const productDetails = document.querySelector(".about-product")
+    const brandDetails = Object.entries(details[0])
+    
+    brandDetails.forEach(detail => {
+        console.log(detail[0] , detail[1])
 
-function renderProductDetails(){
-    const details = getProductDeatls()
-    console.log(details)
+    })
+
+
+    
+ 
+
 
 
 }
 
-renderProductDetails()
+getProductDetails()
