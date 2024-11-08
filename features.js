83edfa048f527a1ce20ddecbe7cc47b567b2caac
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2024-11-08 14:33:29 +0200

    rendered the features

--- features.js
+++ features.js
@@ -1,5 +1,6 @@
 const base_path = "features.json"
 
+// function to get features from json file
 function getFeatures(path){
     fetch(path)
     .then(response => {
@@ -9,15 +10,68 @@ function getFeatures(path){
     return response.json()
 
     }).then(data => {
-        console.log(data)
+       
+        renderFeatures(data)
     })
 
 
 }
+
+// calling features function to render into the DOM
+
 function youMightAlsoLike(){
     getFeatures(base_path)
     
    
 }
 
-youMightAlsoLike(0)
+function renderFeatures (features) {
+    const my_features  = features["products"]
+    const features_div = document.querySelector(".features-rendered")
+
+    console.log(my_features)
+    // rendering my features 
+    my_features.forEach(item => {
+        const {name , description , price , ratings , category ,stock , image } = item
+
+    const card = document.createElement("div")
+    
+    const description__tag = document.createElement("h3")
+    const features_image = document.createElement("img")
+    const price_tag = document.createElement("p")
+    const product_ratings = document.createElement("div")
+    const stock_left = document.createElement("span")
+    card.classList.add("features-card")
+     
+
+    // Elemenets classes
+    features_image.classList.add("features-img")
+
+
+
+
+    // details
+ 
+    features_image.src = `${image}`
+    description__tag.textContent = `${description}`
+    price_tag.textContent = `${price}`
+    product_ratings.textContent = `${ratings}`
+    stock_left.textContent = `${stock}`
+
+
+  
+    //  appending to card
+
+
+    card.append(features_image , description ,price_tag , product_ratings , stock_left)
+    features_div.append(card)
+        
+    });
+
+
+   
+   
+
+
+}
+youMightAlsoLike()
