5673ac1388b643e630b3b494cdfd72ed73c931db
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2024-11-08 14:00:32 +0200

    added a base path

--- features.js
+++ features.js
@@ -1,4 +1,23 @@
+const base_path = "features.json"
+
+function getFeatures(path){
+    fetch(path)
+    .then(response => {
+        if(! response.ok){
+            return new Error("Error network response not found" , response.status)
+        }
+    return response.json()
+
+    }).then(data => {
+        console.log(data)
+    })
+
+
+}
 function youMightAlsoLike(){
+    getFeatures(base_path)
     
    
 }
+
+youMightAlsoLike(0)
