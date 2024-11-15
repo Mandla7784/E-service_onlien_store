1f6d6366b33526b353e9fb500d5090e8324a270c
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-11-15 09:40:50 +0200

    updates

--- about.js
+++ about.js
@@ -1,48 +1,43 @@
 // Product Details
 
-
 // function to fetch data from json file
-function getProductDetails(){
+function getProductDetails() {
+  fetch("data.json")
+    .then((resposne) => {
+      if (!resposne.ok) {
+        throw new Error("HTTP eror", resposne.status);
+      }
 
- fetch("data.json")
-    .then(resposne => {
-        if(!resposne.ok){
-            throw new Error("HTTP eror", resposne.status)
-        }
-       
-         return resposne.json()
-    }).then(data => {
-        console.log(data)
-         renderProductDetails(data) // call render and parse data
+      return resposne.json();
     })
- 
+    .then((data) => {
+      console.log(data);
+      renderProductDetails(data); // call render and parse data
+    });
 }
 //function to display product details
-function renderProductDetails(data){
-    const details =  data // product details list
-    const productDetails = document.querySelector(".about-product")
-    const brandDetails = Object.entries(details[0])
-    
-    brandDetails.forEach(detail => {
-        console.log(detail[0], detail[1]);
-        
-        const html = `
+function renderProductDetails(data) {
+  const details = data; // product details list
+  const productDetails = document.querySelector(".about-product");
+  const brandDetails = Object.entries(details[0]);
+
+  brandDetails.forEach((detail) => {
+    console.log(detail[0], detail[1]);
+
+    const html = `
             <p class="details"> <span class="span">${detail[0]}</span>: ${detail[1]}</p>
         `;
-    
-        // Append the new HTML to the existing content
-        productDetails.innerHTML += html; 
-    });
-     
-    // about this item
 
-    const aboutDetails = Object.entries(details[1])
-    let detail = aboutDetails[0]
-    let header = detail["AboutThisItem"]
-    console.log(header)
-  
+    // Append the new HTML to the existing content
+    productDetails.innerHTML += html;
+  });
 
+  // about this item
 
+  const aboutDetails = Object.entries(details[1]);
+  let detail = aboutDetails[0];
+  let header = detail["AboutThisItem"];
+  console.log(header);
 }
 
-getProductDetails()
+getProductDetails();
--- auth.js
+++ auth.js
@@ -1,12 +1,11 @@
+//User Subscription
 
-  //User Subscription
-
-  const user_email = document.querySelector(".user_subscription_email")
-  const subscribingBUtton  = document.querySelector("#submit-subscriprion")
-   subscribingBUtton.addEventListener("click",function(){
-     if(user_email.value ){
-      page.classList.remove("hide")
-     }else{
-       console.log("Please enter email first")
-     }
-   })
+const user_email = document.querySelector(".user_subscription_email");
+const subscribingBUtton = document.querySelector("#submit-subscriprion");
+subscribingBUtton.addEventListener("click", function () {
+  if (user_email.value) {
+    page.classList.remove("hide");
+  } else {
+    console.log("Please enter email first");
+  }
+});
