6fe8eb48a9b7e376b0623716cc584622b8a51e12
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 05:51:53 +0200

    removed other divs from the cart html file

--- cartPage.js
+++ cartPage.js
@@ -1,11 +1,14 @@
 const cartToggler = document.querySelector(".cart-icon");
+const 
 /**
  * This function is triggered when a cart icon i clicked
  * it subscribes from localstorage and parese tha data to render in the DOM
  */
 function viewCart() {
   const cartItems = localStorage.getItem("cartitems");
-  console.log(JSON.parse(cartItems));
+
+  const jsonCartitems = JSON.parse(cartItems); // parsing to JASON
+
 }
 
 // events on cartToggler
