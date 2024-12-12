938b634ef9dd5c1cea895f7f48100b701a658b5f
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 05:53:29 +0200

    Updates

--- cartPage.js
+++ cartPage.js
@@ -1,5 +1,5 @@
 const cartToggler = document.querySelector(".cart-icon");
-const 
+const shopcartitems = document.querySelector(".shop-cart-items");
 /**
  * This function is triggered when a cart icon i clicked
  * it subscribes from localstorage and parese tha data to render in the DOM
@@ -8,7 +8,7 @@ function viewCart() {
   const cartItems = localStorage.getItem("cartitems");
 
   const jsonCartitems = JSON.parse(cartItems); // parsing to JASON
-
+  shopcartitems.append(jsonCartitems);
 }
 
 // events on cartToggler
