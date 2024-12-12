67743b63fc96c8126ecc5fd97e4b89a004824bb8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 05:27:19 +0200

    Working on bugs

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
   // hero image
   const heroProduct = document.querySelector(".hero-image");
-  // whistle-page
-  const page = document.querySelector(".whistle-page");
+  const btnShop = document.querySelector(".btn-start-shop");
+  const homePage = document.querySelector(".home-page");
+
+  /**
+   * The home page should display none when we activate on the shop now button
+   */
+  btnShop.addEventListener("click", () => {
+    console.log("clciked shopnow");
+    homePage.classList.remove("home-page");
+    homePage.classList.add("home-page-hide");
+  });
+
   // Function to chnage image
   function chnageImage(e) {
     let image_source = e.target;
@@ -13,12 +23,5 @@ window.addEventListener("DOMContentLoaded", () => {
   const headsets = document.querySelectorAll(".heads");
   headsets.forEach((headset) => {
     headset.addEventListener("click", chnageImage);
-  });
-  // Log in FORM
-  const btnFormQuit = document.querySelector(".form-quite");
-  btnFormQuit.addEventListener("click", function () {
-    console.log("clicked");
-    page.classList.add("hide");
-    page.classList.remove("flex");
   });
 });
