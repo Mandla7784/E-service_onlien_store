90a2400366ca3f52164b192819e1c89603535e23
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-16 13:39:04 +0200

    working on delete button

--- cartPage.js
+++ cartPage.js
@@ -16,47 +16,46 @@ const shopcartitems = document.querySelector(".shop-cart-items");
  * this function removes an item by id from localstorage
  */
 function deleteitem(itemkey, listOfItems) {
-  listOfItems.pop(itemkey);
   let updatedItems = listOfItems.filter((item) => item.id != itemkey);
   localStorage.setItem("cartitems", JSON.stringify(updatedItems));
   console.log(updatedItems);
+  viewCart(updatedItems);
 }
+
 /**
  * This function is triggered when a cart icon i clicked
  * it subscribes from localstorage and parese tha data to render in the DOM
  */
-
 function viewCart(cartItems) {
-  const totalTag = document.createElement("h2"); //grand toatl tag
+  const totalTag = document.createElement("h2"); //grand total tag
   shopCartDiv.classList.remove("none");
-  shopcartitems.append(totalTag);
-  cartItems.forEach((item) => {
-    console.log(item.id);
-    const { pri, name, img, id } = item;
-    let totalPrice = 0;
+  shopcartitems.innerHTML = "";
+  let totalPrice = 0;
 
-    const price = Number(pri.slice(1, 5));
+  cartItems.forEach((item) => {
+    const { pri, name, img, id } = item;
+
+    const price = Number(pri.slice(1));
     totalPrice += price;
 
     shopcartitems.innerHTML += /*html*/ `
-  <div class="cart-card">
-    <h4>${name}</h4>
-    <p>R${price.toFixed(2)}</p>
-    <img style="inline-size: 100px; block-size: 100px;" src="${img}" />
-    <button 
-    onclick="deleteitem(${id} , ${JSON.stringify(cartItems)})"
-     
-      class="deletebutton" 
-      style="background-color: rgb(212, 42, 42); color: white;">
-      delete
-    </button>
-  </div>;
-
-     `;
-    return totalPrice;
+      <div class="cart-card">
+        <h4>${name}</h4>
+        <p>R${price.toFixed(2)}</p>
+        <img style="inline-size: 100px; block-size: 100px;" src="${img}" />
+        <button 
+          onclick="(function(){ deleteitem(${id}, jsonCartitems) })()"
+          class="deletebutton" 
+          style="background-color: rgb(212, 42, 42); color: white;">
+          delete
+        </button>
+      </div>
+    `;
   });
-  // totalTag.innerHTML = `Grand TOTAL: ${}`;
+
+  totalTag.innerHTML = `Grand TOTAL: R${totalPrice.toFixed(2)}`;
+  shopcartitems.append(totalTag);
 }
 
 // events on cartToggler
-cartToggler.onclick = viewCart(jsonCartitems);
+cartToggler.onclick = () => viewCart(jsonCartitems);
