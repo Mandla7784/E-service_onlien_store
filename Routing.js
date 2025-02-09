60f1907d22dda0917b1b244b7a2e8b40f4760f8f
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-02-09 16:30:43 +0200

    fixing routing issues

--- Routing.js
+++ Routing.js
@@ -10,7 +10,6 @@ function showPage(pageId) {
   pages.forEach((page) => {
     page.classList.remove("active"); // hiding all pages
   });
-
   const activePage = document.getElementById(pageId);
   if (activePage) {
     activePage.classList.add("active");
