ebf0b33071d03b7d051186e7290fd501da4e68be
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-02-11 15:19:34 +0200

    added a function to create card and append to the DOM

--- products.js
+++ products.js
@@ -4,23 +4,28 @@ const url_base_path = "https://fakestoreapi.com/products";
 const getProducts = (path) => {
   fetch(path)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => createCard(data))
     .catch((e) => console.log(e))
     .finally("products fetched");
 };
 
 getProducts(url_base_path);
 
-const new_ITEMCARD = new ItemCard(
-  "dfg",
-  "dfg",
-  "sdvgfd",
-  "dfg",
-  "dsfdgb",
-  "df",
-  "7"
-);
+function createCard(data) {
+  data.forEach((item) => {
+    // console.log(item);
+    const { catergory, id, title, image, price, rating, description } = item;
 
-const product_placement = document.querySelector(".products_aligning");
-product_placement.append(new_ITEMCARD);
-document.body.append(product_placement);
+    const new_ITEMCARD = new ItemCard(
+      catergory,
+      description,
+      id,
+      image,
+      price,
+      rating
+    );
+    product_placement.append(new_ITEMCARD);
+
+    const product_placement = document.querySelector(".products_aligning");
+  });
+}
